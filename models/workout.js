dfe886a1f73da0221865ca09a2d03c49902fe6c2
Blaise <ifollowjre@gmail.com>
2020-03-25 18:52:58 -0600

    just little by little decoding the ever so sleight bugs there was left for us

---
+++ models/workout.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: { type: Number, required: true },
+  exercises: [
+    {
+      type: { type: String, required: true },
+      name: { type: String, required: true },
+      duration: { type: Number, required: true },
+      weight: { type: Number, required: true },
+      reps: { type: Number, required: true },
+      sets: { type: Number, required: true }
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+// console.log(Workout);
+module.exports = Workout;
