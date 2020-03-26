eab7b77416e9eeefb8b5ccb3cdfc668cacb9d175
Blaise <ifollowjre@gmail.com>
2020-03-25 19:21:41 -0600

    ah huh

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true
+});
 
 // routes
 // require("./models")(app);
